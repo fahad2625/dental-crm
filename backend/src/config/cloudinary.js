23184fe0d5ccf2416eb8088a2b698abf4a195190
fahad2625 <fahadbasheer02@gmail.com>
2026-02-25 14:12:16 +0530

    new uploaddddddd

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+ CLOUDINARY_CLOUD_NAME: process.env.CLOUD_NAME,
+  CLOUDINARY_API_SECRET: process.env.API_KEY,
+  CLOUDINARY_API_SECRET: process.env.API_SECRET,
 });
 
 export default cloudinary;
